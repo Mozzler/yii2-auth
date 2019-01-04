2a8281781a06c0adbbc7b734d9327ecb5fa178de
Michael Kubler <michael@greyphoenix.biz>
2019-01-04 11:33:04 +1030

    Set an "Invalid Username or Password" flash and return false if you try to login with the wrong email address.

--- actions/UserLoginAction.php
+++ actions/UserLoginAction.php
@@ -7,36 +7,45 @@ use yii\web\ViewAction;
 class UserLoginAction extends \mozzler\base\actions\BaseModelAction {
 
 	public $id = 'login';
-	
+
 	public function run() {
 		if (!Yii::$app->user->isGuest) {
             return $this->goHome();
         }
-        
+
         $model = \Yii::createObject(\Yii::$app->user->identityClass);
         $model->setScenario($model::SCENARIO_LOGIN);
-        
+
         if ($model->load(Yii::$app->request->post()) && $this->login($model)) {
             return $this->controller->goBack();
         }
 
         $model->password = '';
-        
+
         $this->controller->data['model'] = $model;
-		
+
 		return parent::run();
 	}
-	
+
 	protected function login($model) {
 		$usernameField = $model::$usernameField;
 		$user = $model::findByUsername($model->$usernameField);
+
+		// -- Invalid user
+		if (empty($user)) {
+            Yii::$app->session->setFlash('error', "Invalid Username or Password");
+		    return false;
+        }
+
 		$valid = $user->validatePassword($model->password);
-		
+
 		if ($valid) {
 			Yii::$app->user->login($user, 0);
-		}
-		
+		} else {
+            Yii::$app->session->setFlash('error', "Invalid Username and/or Password");
+        }
+
 		return $valid;
 	}
-	
+
 }
