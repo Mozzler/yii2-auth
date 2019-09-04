8aaa4907da46aac18966a2022bae749c7b32b324
kublermdk <michael@filmsonthefly.com>
2019-09-04 20:35:00 +0800

    * Ensure the User web controller has decent RBAC as it's not applying the base fields like previously

--- controllers/UserWebController.php
+++ controllers/UserWebController.php
@@ -1,36 +1,89 @@
 <?php
+
 namespace mozzler\auth\controllers;
 
 use mozzler\base\controllers\ModelController;
 
-class UserWebController extends ModelController {
+class UserWebController extends ModelController
+{
 
-	public $modelClass = 'mozzler\auth\models\User';
-	public static $moduleClass = 'mozzler\auth\Module';
-	
-	public function actions() {
-		return \yii\helpers\ArrayHelper::merge(parent::actions(), [
-			'login' => 'mozzler\auth\actions\UserLoginAction',
-			'logout' => 'mozzler\auth\actions\UserLogoutAction',
-			'requestPasswordReset' => 'mozzler\auth\actions\UserRequestPasswordReset',
-			'passwordReset' => 'mozzler\auth\actions\UserPasswordReset'
-		]);
-	}
-	
-	public static function rbac() {
-		return [
-			'registered' => [
-				'create' => [
-					'grant' => false
-				],
-				'update' => [
-					'grant' => true
-				],
-				'delete' => [
-					'grant' => false
-				]
-			]
-		];
-	}
-	
+    public $modelClass = 'mozzler\auth\models\User';
+    public static $moduleClass = 'mozzler\auth\Module';
+
+    public function actions()
+    {
+        return \yii\helpers\ArrayHelper::merge(parent::actions(), [
+            'login' => 'mozzler\auth\actions\UserLoginAction',
+            'logout' => 'mozzler\auth\actions\UserLogoutAction',
+            'requestPasswordReset' => 'mozzler\auth\actions\UserRequestPasswordReset',
+            'passwordReset' => 'mozzler\auth\actions\UserPasswordReset'
+        ]);
+    }
+
+
+    public static function rbac()
+    {
+        return [
+            'public' => [
+                'create' => [
+                    'grant' => false
+                ],
+                'view' => [
+                    'grant' => false
+                ],
+                'update' => [
+                    'grant' => false
+                ],
+                'index' => [
+                    'grant' => false
+                ],
+                'delete' => [
+                    'grant' => false
+                ],
+                'deleteMany' => [
+                    'grant' => false
+                ]
+            ],
+            'admin' => [
+                'create' => [
+                    'grant' => true
+                ],
+                'view' => [
+                    'grant' => true
+                ],
+                'update' => [
+                    'grant' => true
+                ],
+                'index' => [
+                    'grant' => true
+                ],
+                'delete' => [
+                    'grant' => false
+                ],
+                'deleteMany' => [
+                    'grant' => false
+                ]
+            ],
+            'registered' => [
+                'create' => [
+                    'grant' => false
+                ],
+                'view' => [
+                    'grant' => true
+                ],
+                'update' => [
+                    'grant' => true
+                ],
+                'index' => [
+                    'grant' => true
+                ],
+                'delete' => [
+                    'grant' => false
+                ],
+                'deleteMany' => [
+                    'grant' => false
+                ]
+            ]
+        ];
+    }
 }
